5826ccffd01598d129bdd1432469a13ae8590b13
Offuru <andrei.rozmarin@gmail.com>
2024-04-11 14:16:32 +0300

    added initial project files

---
+++ Aplicatie-de-Gestiune-a-Obiectelor-Eletronice/Aplicatie-de-Gestiune-a-Obiectelor-Eletronice/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Aplicatie_de_Gestiune_a_Obiectelor_Eletronice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
