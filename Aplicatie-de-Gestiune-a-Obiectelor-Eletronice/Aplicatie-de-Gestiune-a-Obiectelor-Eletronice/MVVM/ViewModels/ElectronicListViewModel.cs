5856901c9cfc6ffe9cb0c44ee27c62a093fb9a4a
Stanica Dragos <50087496+stdragos@users.noreply.github.com>
2024-04-16 15:15:38 +0300

    Added datagrid for ElectronicList Page

--- Aplicatie-de-Gestiune-a-Obiectelor-Eletronice/Aplicatie-de-Gestiune-a-Obiectelor-Eletronice/MVVM/ViewModels/ElectronicListViewModel.cs
+++ Aplicatie-de-Gestiune-a-Obiectelor-Eletronice/Aplicatie-de-Gestiune-a-Obiectelor-Eletronice/MVVM/ViewModels/ElectronicListViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aplicatie_de_Gestiune_a_Obiectelor_Eletronice.Core;
+using Aplicatie_de_Gestiune_a_Obiectelor_Eletronice.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace Aplicatie_de_Gestiune_a_Obiectelor_Eletronice.ViewModels
 {
     class ElectronicListViewModel : Core.ViewModel
     {
+        private INavigationService _navigation;
+
+        public INavigationService Navigation
+        {
+            get => _navigation;
+            set
+            {
+                _navigation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand NavigateToMenuCommand { get; set; }
+        public ElectronicListViewModel(INavigationService navService)
+        {
+            Navigation = navService;
+            NavigateToMenuCommand = new RelayCommand(o => { Navigation.NavigateTo<MenuViewModel>(); }, o => true);
+        }
     }
 }
